685e40e31f0f04ff80057b392f48e74842ae100d
李博修 <liboxiu@dreamofalice.club>
2021-02-09 13:28:03 +0800

    done account

--- mo2front/src/components/AccountModal.vue
+++ mo2front/src/components/AccountModal.vue
@@ -9,42 +9,97 @@
       <v-container>
         <v-row>
           <v-col cols="12">
-            <v-card-title> 登录 </v-card-title>
+            <v-card-title>
+              <v-tabs align-with-title v-model="tabkey">
+                <v-tab :key="1">登录</v-tab>
+                <v-tab :key="2">注册</v-tab>
+              </v-tabs>
+            </v-card-title>
           </v-col>
         </v-row>
-        <v-card-text>
-          <v-row>
-            <v-col cols="12">
-              <v-text-field
-                label="Email"
-                v-model="email"
-                :rules="validateEmail()"
-              >
-                <v-icon slot="append" color="gray"> mdi-email </v-icon>
-              </v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12">
-              <v-text-field
-                label="Password"
-                v-model="password"
-                :type="showPasswd ? 'text' : 'password'"
-                :append-icon="showPasswd ? 'mdi-eye-off' : 'mdi-eye'"
-                @click:append="showPasswd = !showPasswd"
-                hint="长度最小为8"
-                :rules="validatePasswd()"
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-switch label="记住我"></v-switch>
-          <v-spacer></v-spacer>
-          <v-btn outlined text>登录</v-btn>
-          <v-btn color="red">取消</v-btn>
-        </v-card-actions>
+        <v-tabs-items v-model="tabkey">
+          <v-tab-item :key="1">
+            <v-card-text>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Email"
+                    v-model="email"
+                    :rules="validateEmail()"
+                  >
+                    <v-icon slot="append" color="gray"> mdi-email </v-icon>
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password"
+                    v-model="password"
+                    :type="showPasswd ? 'text' : 'password'"
+                    :append-icon="showPasswd ? 'mdi-eye-off' : 'mdi-eye'"
+                    @click:append="showPasswd = !showPasswd"
+                    hint="长度最小为8"
+                    :rules="validatePasswd()"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-switch label="记住我"></v-switch>
+              <v-spacer></v-spacer>
+              <v-btn outlined text>{{ reg ? "注册&" : "" }}登录</v-btn>
+              <v-btn @click="close" color="red">取消</v-btn>
+            </v-card-actions>
+          </v-tab-item>
+          <v-tab-item :key="2">
+            <v-card-text>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Name"
+                    v-model="name"
+                    :rules="validateName()"
+                  >
+                    <v-icon slot="append" color="gray"> mdi-account </v-icon>
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Email"
+                    v-model="email"
+                    :rules="validateEmail()"
+                  >
+                    <v-icon slot="append" color="gray"> mdi-email </v-icon>
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password"
+                    v-model="password"
+                    :type="showPasswd ? 'text' : 'password'"
+                    :append-icon="showPasswd ? 'mdi-eye-off' : 'mdi-eye'"
+                    @click:append="showPasswd = !showPasswd"
+                    hint="长度最小为8"
+                    :rules="validatePasswd()"
+                  >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-switch label="记住我"></v-switch>
+              <v-spacer></v-spacer>
+              <v-btn outlined text>注册&登录</v-btn>
+              <v-btn @click="close" color="red">取消</v-btn>
+            </v-card-actions>
+          </v-tab-item>
+        </v-tabs-items>
       </v-container>
     </v-card>
   </v-dialog>
@@ -66,7 +121,9 @@ export default class AccountModal extends Vue {
   @Prop()
   enable!: boolean;
   email: string = "";
+  name: string = "";
   password: string = "";
+  tabkey = 1;
   validator = {
     password: {
       required: required,
@@ -76,7 +133,11 @@ export default class AccountModal extends Vue {
       required: required,
       email: email,
     },
+    name: {
+      required: required,
+    },
   };
+  reg = false;
   showPasswd: boolean = false;
   created() {
     this.email = "";
@@ -84,6 +145,11 @@ export default class AccountModal extends Vue {
   }
   close() {
     this.$emit("update:enable", false);
+  }
+  validateName() {
+    this.$v.name.$touch();
+
+    return [() => this.$v.name.required || "密码不可为空"];
   }
 
   validatePasswd() {
