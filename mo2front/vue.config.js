f82de66b647ffc5b1683ee54b03dc86eb8732cee
李博修 <liboxiu@dreamofalice.club>
2021-02-18 22:10:24 +0800

    update config

--- mo2front/vue.config.js
+++ mo2front/vue.config.js
@@ -39,9 +39,9 @@ module.exports = {
         pathRewrite: {
           '^/frontend': ''
         },
-        secure: true, // 使用的是http协议则设置为false，https协议则设置为true
+        secure: false, // 使用的是http协议则设置为false，https协议则设置为true
         // 开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样客户端端和服务端进行数据的交互就不会有跨域问题
-        changOrigin: true,
+        // changOrigin: true,
         // cookieDomainRewrite: "localhost"
       },
       // '/img': {
