9b0c9373284e5c61c25d32ecaa183be97b442bfe
李博修 <liboxiu@dreamofalice.club>
2021-02-21 22:54:52 +0800

    done read article

--- mo2front/src/router/index.ts
+++ mo2front/src/router/index.ts
@@ -25,6 +25,22 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Account.vue')
+  },
+  {
+    path: '/article/:id?',
+    name: 'Read Article',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ReadArticle.vue')
+  },
+  {
+    path: '/edit/:id?',
+    name: 'Edit Article',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EditArticle.vue')
   }
 ]
 
