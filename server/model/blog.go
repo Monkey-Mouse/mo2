792012a0fc6afab2e3baff4cb7f2525b17d96c36
cascara <easilylazy@qq.coom>
2021-02-24 20:11:13 +0800

    fix bugs in category ids in blog

--- server/model/blog.go
+++ server/model/blog.go
@@ -8,26 +8,26 @@ import (
 // 若修改字段，需注意依赖此model的使用地方
 // important: dto.QueryBlog UpsertBlog()
 type Blog struct {
-	ID          primitive.ObjectID `json:"id,omitempty" example:"xxxxxxxxxxxxx==" bson:"_id,omitempty"`
-	AuthorID    primitive.ObjectID `json:"authorId,omitempty" example:"xxxxxxxxxxxxx==" bson:"author_id"`
-	Title       string             `json:"title,omitempty" example:"mouse ❤ monkey" bson:"title,omitempty"`
-	Description string             `json:"description,omitempty" example:"mouse ❤ monkey" bson:"description,omitempty"`
-	Content     string             `json:"content,omitempty" example:"xxxx\nxxxx" bson:"content,omitempty"`
-	EntityInfo  Entity             `json:"entityInfo,omitempty" bson:"entity_info,omitempty"`
-	Cover       string             `json:"cover,omitempty" example:"https://xxx/xxx" bson:"cover,omitempty"`
-	KeyWords    []string           `json:"keyWords,omitempty" example:"xxx,xxx" bson:"key_words,omitempty"`
-	Categories  []Category         `json:"categories,omitempty" bson:"categories,omitempty"`
+	ID          primitive.ObjectID   `json:"id,omitempty" example:"xxxxxxxxxxxxx==" bson:"_id,omitempty"`
+	AuthorID    primitive.ObjectID   `json:"authorId,omitempty" example:"xxxxxxxxxxxxx==" bson:"author_id"`
+	Title       string               `json:"title,omitempty" example:"mouse ❤ monkey" bson:"title,omitempty"`
+	Description string               `json:"description,omitempty" example:"mouse ❤ monkey" bson:"description,omitempty"`
+	Content     string               `json:"content,omitempty" example:"xxxx\nxxxx" bson:"content,omitempty"`
+	EntityInfo  Entity               `json:"entityInfo,omitempty" bson:"entity_info,omitempty"`
+	Cover       string               `json:"cover,omitempty" example:"https://xxx/xxx" bson:"cover,omitempty"`
+	KeyWords    []string             `json:"keyWords,omitempty" example:"xxx,xxx" bson:"key_words,omitempty"`
+	CategoryIDs []primitive.ObjectID `json:"categories,omitempty" bson:"categories,omitempty"`
 }
 
 func (b *Blog) Init() {
 	b.ID = primitive.NewObjectID()
 	b.EntityInfo = InitEntity()
 }
-func (b *Blog) Add2Category(category Category) {
-	b.Categories = append(b.Categories, category)
+func (b *Blog) Add2Category(categoryID primitive.ObjectID) {
+	b.CategoryIDs = append(b.CategoryIDs, categoryID)
 }
-func (b *Blog) Add2Categories(categories []Category) {
-	b.Categories = append(b.Categories, categories...)
+func (b *Blog) Add2Categories(categoryIDs []primitive.ObjectID) {
+	b.CategoryIDs = append(b.CategoryIDs, categoryIDs...)
 }
 func (b *Blog) IsValid() (valid bool) {
 	valid = true
