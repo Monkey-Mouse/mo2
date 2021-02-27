f80268c2412c0b99c82612cd67119ff586350b8b
李博修 <liboxiu@dreamofalice.club>
2021-02-27 11:54:27 +0800

    fix qiniu dupkey

--- mo2img/qiniu.go
+++ mo2img/qiniu.go
@@ -1,6 +1,7 @@
 package mo2img
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/qiniu/go-sdk/v7/auth/qbox"
@@ -11,10 +12,24 @@ var accessKey = os.Getenv("qiniuak")
 var secretKey = os.Getenv("qiniusk")
 var mac = qbox.NewMac(accessKey, secretKey)
 
+const bucket = "mo2"
+
 // GenerateUploadToken generate qiniu upload token
 func GenerateUploadToken(saveKey string) (token string) {
 	var putPolicy = storage.PutPolicy{
-		Scope:        "mo2",
+		Scope:        bucket,
+		Expires:      7200,
+		FsizeLimit:   1024 * 1024 * 20,
+		SaveKey:      saveKey,
+		ForceSaveKey: true,
+	}
+	return putPolicy.UploadToken(mac)
+}
+
+// GenerateOverwriteToken generate qiniu upload token
+func GenerateOverwriteToken(saveKey string, overwriteKey string) (token string) {
+	var putPolicy = storage.PutPolicy{
+		Scope:        fmt.Sprintf("%s:%s", bucket, overwriteKey),
 		Expires:      7200,
 		FsizeLimit:   1024 * 1024 * 20,
 		SaveKey:      saveKey,
