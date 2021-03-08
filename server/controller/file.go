dc2df23be3586f8540fbbc3a7a1e2e197469ea18
leezeeyee <easilylazy@qq.com>
2021-03-08 14:35:06 +0800

    connect import md file with api

--- server/controller/file.go
+++ server/controller/file.go
@@ -3,9 +3,14 @@ package controller
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
+	"io/ioutil"
 	_ "mime/multipart" //in godoc comment
+	"mo2/database"
+	"mo2/mo2utils"
+	"mo2/server/controller/badresponse"
+	"mo2/services/importService"
 	"net/http"
+	"time"
 )
 
 // Upload godoc
@@ -14,18 +19,33 @@ import (
 // @Accept multipart/form-data
 // @Produce  json
 // @Param form body string true "file"
-// @Success 200 {string} json
-// @Router /file [post]
+// @Success 200 {object} model.Blog
+// @Failure 400 {object} badresponse.ResponseError
+// @Failure 401 {object} badresponse.ResponseError
+// @Failure 404 {object} badresponse.ResponseError
+// @Router /api/file [post]
 func (c *Controller) Upload(ctx *gin.Context) {
-	// Multipart form
-	form, _ := ctx.MultipartForm()
-	files := form.File["upload[]"]
-
-	for _, file := range files {
-		log.Println(file.Filename)
-		//importService.Transform(file)
-		// Upload the file to specific dst.
-		// ctx.SaveUploadedFile(file, "\\foo")
+	userInfo, exist := mo2utils.GetUserInfo(ctx)
+	if !exist {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, badresponse.SetResponseReason("请先登录"))
 	}
-	ctx.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
+	// Multipart form
+	file, _ := ctx.FormFile("upload[]")
+
+	t1 := time.Now()
+	src, err := file.Open()
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, badresponse.SetResponseError(err))
+	}
+	defer src.Close()
+	data, err := ioutil.ReadAll(src)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, badresponse.SetResponseError(err))
+	}
+	blog := importService.Transform(data)
+	fmt.Println(time.Since(t1))
+	blog.AuthorID = userInfo.ID
+	isDraft := true
+	database.UpsertBlog(&blog, isDraft)
+	ctx.JSON(http.StatusOK, blog)
 }
