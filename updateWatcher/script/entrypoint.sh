25c745a351cd2cf33f4e3dec57a89ccd5eb511f4
李博修 <liboxiu@dreamofalice.club>
2021-03-09 15:48:13 +0800

    epic: domain&ssl

---
+++ updateWatcher/script/entrypoint.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+echo "start nginx"
+
+#set TZ
+if [[ ! -z "${TZ}" ]]; then
+    cp /usr/share/zoneinfo/${TZ} /etc/localtime
+    echo ${TZ} >/etc/timezone
+fi
+
+#setup ssl keys, export to pass them to le.sh
+echo "ssl_key=${SSL_KEY:=le-key.pem}, ssl_cert=${SSL_CERT:=le-crt.pem}, ssl_chain_cert=${SSL_CHAIN_CERT:=le-chain-crt.pem}"
+export LE_SSL_KEY=/etc/nginx/ssl/${SSL_KEY}
+export LE_SSL_CERT=/etc/nginx/ssl/${SSL_CERT}
+export LE_SSL_CHAIN_CERT=/etc/nginx/ssl/${SSL_CHAIN_CERT}
+
+#create destination folders
+mkdir -p /etc/nginx/conf.d
+mkdir -p /etc/nginx/ssl
+
+#collect services
+SERVICES_FILES=$(find "/etc/nginx/" -type f -maxdepth 1 -name "service*.conf")
+
+#copy service*.conf from /etc/nginx/ if they are mounted
+if [ ${#SERVICES_FILES} -ne 0 ]; then
+    cp -fv /etc/nginx/service*.conf /etc/nginx/conf.d/
+fi
+
+#replace SSL_KEY, SSL_CERT and SSL_CHAIN_CERT by actual keys
+sed -i "s|SSL_KEY|${LE_SSL_KEY}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+sed -i "s|SSL_CERT|${LE_SSL_CERT}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+sed -i "s|SSL_CHAIN_CERT|${LE_SSL_CHAIN_CERT}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+
+#replace LE_FQDN
+sed -i "s|LE_FQDN|${LE_FQDN}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+
+#generate dhparams.pem
+if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
+    echo "make dhparams"
+    cd /etc/nginx/ssl
+    openssl dhparam -out dhparams.pem 2048
+    chmod 600 dhparams.pem
+fi
+
+#disable configuration and let it run without SSL
+mv -v /etc/nginx/conf.d /etc/nginx/conf.d.disabled
+
+(
+ sleep 5 #give nginx time to start
+ echo "start letsencrypt updater"
+ while :; do
+    echo "trying to update letsencrypt ..."
+    /le.sh
+    #on the first run remove default config, conflicting on 80
+    rm -f /etc/nginx/conf.d/default.conf 2>/dev/null
+    #on the first run enable config back
+    mv -v /etc/nginx/conf.d.disabled /etc/nginx/conf.d 2>/dev/null
+    echo "reload nginx with ssl"
+    nginx -s reload
+    sleep 10d
+ done
+) &
+
+nginx -g "daemon off;"
---
+++ updateWatcher/script/le.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# scripts is trying to renew certificate only if close (30 days) to expiration
+# returns 0 only if certbot called.
+
+# 30 days
+renew_before=2592000
+
+if [ "$LETSENCRYPT" != "true" ]; then
+    echo "letsencrypt disabled"
+    return 1
+fi
+
+# redirection to /dev/null to remove "Certificate will not expire" output
+if [ -f ${LE_SSL_CERT} ] && openssl x509 -checkend ${renew_before} -noout -in ${LE_SSL_CERT} >/dev/null; then
+    # egrep to remove leading whitespaces
+    CERT_FQDNS=$(openssl x509 -in ${LE_SSL_CERT} -text -noout | egrep -o 'DNS.*')
+    # run and catch exit code separately because couldn't embed $@ into `if` line properly
+    set -- $(echo ${LE_FQDN} | tr ',' '\n')
+    for element in "$@"; do echo ${CERT_FQDNS} | grep -q $element; done
+    CHECK_RESULT=$?
+    if [ ${CHECK_RESULT} -eq 0 ]; then
+        echo "letsencrypt certificate ${LE_SSL_CERT} still valid"
+        return 1
+    fi
+    echo "letsencrypt certificate ${LE_SSL_CERT} is present, but doesn't contain expected domains"
+    echo "expected: ${LE_FQDN}"
+    echo "found:    ${CERT_FQDNS}"
+fi
+
+echo "letsencrypt certificate will expire soon or missing, renewing..."
+certbot certonly -t -n --agree-tos --renew-by-default --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d ${LE_FQDN}
+le_result=$?
+if [ ${le_result} -ne 0 ]; then
+    echo "failed to run certbot"
+    return 1
+fi
+
+FIRST_FQDN=$(echo "$LE_FQDN" | cut -d"," -f1)
+cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/privkey.pem ${LE_SSL_KEY}
+cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/fullchain.pem ${LE_SSL_CERT}
+cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/chain.pem ${LE_SSL_CHAIN_CERT}
+return 0
