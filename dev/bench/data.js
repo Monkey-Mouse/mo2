af5249d4b0b4000f783ab635fa41fa4e9ea7a659
github-action-benchmark <github@users.noreply.github.com>
2021-03-26 02:58:10 +0000

    add Benchmark (go) benchmark result for f03a78ded7680ef9903998caca13867a543ffef4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616725201896,
+  "lastUpdate": 1616727490763,
   "repoUrl": "https://github.com/Monkey-Mouse/mo2",
   "entries": {
     "Benchmark": [
@@ -50,6 +50,44 @@ window.BENCHMARK_DATA = {
           {
             "name": "Benchmark_checkRoles",
             "value": 0.000007,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Monkey-Mouse",
+            "username": "Monkey-Mouse"
+          },
+          "committer": {
+            "name": "Monkey-Mouse",
+            "username": "Monkey-Mouse"
+          },
+          "id": "f03a78ded7680ef9903998caca13867a543ffef4",
+          "message": "Chronostasys/benchmark action",
+          "timestamp": "2021-03-25T04:33:52Z",
+          "url": "https://github.com/Monkey-Mouse/mo2/pull/228/commits/f03a78ded7680ef9903998caca13867a543ffef4"
+        },
+        "date": 1616727490191,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_Middleware_redis",
+            "value": 0.0113,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_Middleware_memory",
+            "value": 0.00051,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          },
+          {
+            "name": "Benchmark_checkRoles",
+            "value": 0.000005,
             "unit": "ns/op",
             "extra": "1000000000 times\n2 procs"
           }
