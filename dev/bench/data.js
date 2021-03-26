048b97d5a6e03feab63fbdc9c37b8da33c286a0f
github-action-benchmark <github@users.noreply.github.com>
2021-03-26 02:11:44 +0000

    add Benchmark (go) benchmark result for c16a3a99030a6e133a207576fba8643c4379e4df

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1616724704670,
+  "repoUrl": "https://github.com/Monkey-Mouse/mo2",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Monkey-Mouse",
+            "username": "Monkey-Mouse"
+          },
+          "committer": {
+            "name": "Monkey-Mouse",
+            "username": "Monkey-Mouse"
+          },
+          "id": "c16a3a99030a6e133a207576fba8643c4379e4df",
+          "message": "Chronostasys/benchmark action",
+          "timestamp": "2021-03-25T04:33:52Z",
+          "url": "https://github.com/Monkey-Mouse/mo2/pull/228/commits/c16a3a99030a6e133a207576fba8643c4379e4df"
+        },
+        "date": 1616724704122,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "Benchmark_checkRoles",
+            "value": 0.000005,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
