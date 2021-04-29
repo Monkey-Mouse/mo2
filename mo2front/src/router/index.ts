258a9ed39505a8ef1af282d161227569e2dc0081
李博修 <liboxiu@dreamofalice.club>
2021-04-29 11:42:25 +0800

    🐛 [fix] front oauth redirect

--- mo2front/src/router/index.ts
+++ mo2front/src/router/index.ts
@@ -75,6 +75,14 @@ const routes: Array<RouteConfig> = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Recycle.vue')
   },
   {
+    path: '/api/*',
+    name: 'Processing',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Processing.vue')
+  },
+  {
     path: '*',
     name: 'Not Found',
     // route level code-splitting
