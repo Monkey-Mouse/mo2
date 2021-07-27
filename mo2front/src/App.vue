ecb4cb1b857f4fbb0bc7900e2a8a2c477bccc7cc
chronos <1769712655@qq.com>
2021-07-27 22:00:32 +0800

    [fix] pwa install

--- mo2front/src/App.vue
+++ mo2front/src/App.vue
@@ -303,7 +303,7 @@
         >冀ICP备20007570号-2</a
       >
     </v-footer>
-    <pwa-install style="display: none"></pwa-install>
+    <pwa-install></pwa-install>
   </v-app>
 </template>
 
@@ -667,6 +667,9 @@ export default class App extends Vue {
 @import "./assets/main.scss";
 </style>
 <style>
+#openButton {
+  display: none !important;
+}
 .clickable {
   cursor: pointer;
 }
